f616ace242c961cb68a7c95a977ce3a44b5e41f9
gladson <gladsonbrito@gmail.com>
2020-04-06 22:46:29 -0400

    refactor(module): alterado local do PokeRepository

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -7,16 +7,14 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:teste_flutter_slidy_modular_mobx/app/app_controller.dart';
 import 'package:teste_flutter_slidy_modular_mobx/app/app_widget.dart';
 import 'package:teste_flutter_slidy_modular_mobx/app/modules/home/home_module.dart';
-import 'package:teste_flutter_slidy_modular_mobx/app/shared/repository/poke_repository.dart';
 import 'package:teste_flutter_slidy_modular_mobx/app/shared/utils/constants.dart';
 
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
-        Bind((i) => AppController()),
-        Bind((i) => PokeRepository(i.get<Dio>())),
-        Bind((i) => Dio(BaseOptions(baseUrl: URL_BASE))),
-      ];
+    Bind((i) => AppController()),
+    Bind((i) => Dio(BaseOptions(baseUrl: URL_BASE))),
+  ];
 
   @override
   List<Router> get routers => [
