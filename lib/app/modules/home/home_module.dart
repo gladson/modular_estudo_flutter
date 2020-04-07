10da8cb87732b2cf6ac9f052b35ac1f0e9785f77
gladson <gladsonbrito@gmail.com>
2020-04-06 22:49:58 -0400

    refactor(module): alterado import poke_repository
    * add Bind PokeRepository
    * clean code Router

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,19 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'package:teste_flutter_slidy_modular_mobx/app/modules/home/home_page.dart';
 import 'package:teste_flutter_slidy_modular_mobx/app/modules/home/home_controller.dart';
-import 'package:teste_flutter_slidy_modular_mobx/app/shared/repository/poke_repository.dart';
+import 'package:teste_flutter_slidy_modular_mobx/app/modules/home/repository/poke_repository.dart';
 
 class HomeModule extends ChildModule {
   @override
   List<Bind> get binds => [
     Bind((i) => HomeController(i.get<PokeRepository>())),
+    Bind((i) => PokeRepository(i.get<Dio>())),
   ];
 
   @override
   List<Router> get routers => [
-        Router(Modular.initialRoute, child: (_, args) => HomePage()),
-      ];
+    Router(Modular.initialRoute, child: (_, args) => HomePage()),
+  ];
 
   static Inject get to => Inject<HomeModule>.of();
 }
